a36eac691678b77458471f359dce95b7b6055bb4
tamse94 <kopetraja254@gmail.com>
2026-04-27 06:45:07 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,18 @@ import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
-      <body>{children}</body>
+    <html lang="id" className="scroll-smooth">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+        {/* Google Material Icons */}
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" rel="stylesheet" />
+      </head>
+      {/* Set font default ke Jost di body */}
+      <body style={{ fontFamily: "'Jost', sans-serif" }} className="bg-slate-950 text-slate-100 min-h-screen selection:bg-blue-500 selection:text-white">
+        {children}
+      </body>
     </html>
   );
 }
