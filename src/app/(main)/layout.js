e12bf7be279f57e72766ecb1e47c038afba73310
tamse94 <kopetraja254@gmail.com>
2026-04-27 08:02:15 +0700

    Update layout.js

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -4,7 +4,7 @@ export default function MainLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col w-full bg-slate-950">
       <Header />
-      <main className="flex-grow w-full px-4 sm:px-6 py-8 max-w-3xl mx-auto">
+      <main className="flex-grow w-full px-1 sm:px-6 py-6 sm:py-8 max-w-3xl mx-auto">
         {children}
       </main>
     </div>
