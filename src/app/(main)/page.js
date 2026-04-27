a1161fb88d30b1ae83d586e13e2e79be1e956df1
tamse94 <kopetraja254@gmail.com>
2026-04-27 08:20:49 +0700

    Update page.js

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,10 +1,14 @@
 import ShortenerForm from '@/components/form/ShortenerForm';
+import UrlList from '@/components/list/UrlList';
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-4 sm:mb-6 md:mb-8 lg:mb-10 text-center text-blue-400">Buat URL Pendek</h1>
+    <div className="w-full flex flex-col gap-4 sm:gap-8 pb-10">
+      {/* Form bagian atas */}
       <ShortenerForm />
+      
+      {/* List tabel bagian bawah */}
+      <UrlList />
     </div>
   );
 }
